033c43f563e22e2e8ec3f9f12640c2e99737d02f
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-10-27 23:15:25 -0400

    adição de StarChart.vue e UserHighlights.vue na estrutura básicba

---
+++ src/components/StarChart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Star Chart Component</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StarChart'
+}
+</script>
+
+<style scoped>
+/* Estilos do StarChart */
+</style>
---
+++ src/components/UserHighlights.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>User Highlights Component</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserHighlights'
+}
+</script>
+
+<style scoped>
+/* Estilos do UserHighlights */
+</style>
